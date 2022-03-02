888598592b12ff979d5570a044618165c6503b11
Your Name <you@example.com>
2022-03-02 12:46:43 +0100

    lab 03 dodelat

---
+++ labs/03-vidado/multiplexer/miltiplexer/miltiplexer.srcs/sim_1/new/tb_mux_3bit_4to1.vhd
@@ -0,0 +1,62 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+------------------------------------------------------------
+-- Entity declaration for testbench
+------------------------------------------------------------
+entity tb_mux_3bit_4to1 is
+    -- Entity of testbench is always empty
+end entity tb_mux_3bit_4to1;
+
+------------------------------------------------------------
+-- Architecture body for testbench
+------------------------------------------------------------
+architecture testbench of tb_mux_3bit_4to1 is
+
+    -- Local signals
+    signal s_a           : std_logic_vector(4 - 1 downto 0);
+    signal s_b           : std_logic_vector(4 - 1 downto 0);
+    signal s_B_greater_A : std_logic;
+    signal s_B_equals_A  : std_logic;
+    signal s_B_less_A    : std_logic;
+
+begin
+    -- Connecting testbench signals with comparator_4bit
+    -- entity (Unit Under Test)
+    uut_mux_3bit_4to1 : entity work.mux_3bit_4to1
+        port map(
+            a_i           => s_a,
+            b_i           => s_b,
+            B_greater_A_o => s_B_greater_A,
+            B_equals_A_o  => s_B_equals_A,
+            B_less_A_o    => s_B_less_A
+        );
+
+    --------------------------------------------------------
+    -- Data generation process
+    --------------------------------------------------------
+    p_stimulus : process
+    begin
+        -- Report a note at the beginning of stimulus process
+        report "Stimulus process started" severity note;
+
+
+        -- My test case for "xxxx33"
+        s_b <= "0011"; 
+        s_a <= "0011";       
+        wait for 100 ns;
+        -- Expected output
+        assert ((s_B_greater_A = '0') and
+                (s_B_equals_A  = '1') and
+                (s_B_less_A    = '0'))
+        -- If false, then report an error
+        report "Input combination has failed" severity error;
+
+
+
+        -- Report a note at the end of stimulus process
+        report "Stimulus process finished" severity note;
+        wait; -- Data generation process is suspended forever
+    end process p_stimulus;
+
+end architecture testbench;
